178fee43ab4134a8449c1beb68c3893bdb219f9f
JiriLojda <jiril@kentico.com>
2019-05-10 10:57:16 +0200

    Make ship shoot

--- frontend/src/core/strategyCore/enums/worldObject.ts
+++ frontend/src/core/strategyCore/enums/worldObject.ts
@@ -1,7 +1,18 @@
+import {List} from "immutable";
+
 export enum WorldObject {
     Ship = 'S',
     Diamond = 'D',
     Meteoroid = 'M',
     Asteroid = 'A',
     Wormhole = 'W',
+    Laser = 'laser',
+    LaserStart = 'laser-start',
+    LaserEnd = 'laser-end',
 }
+
+export const destructableObjects: List<WorldObject> = List([
+    WorldObject.Ship,
+    WorldObject.Meteoroid,
+    WorldObject.Asteroid,
+]);
