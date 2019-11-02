2f52fb5571a78c6b31ef186e216f02667157eb74
JiriLojda <jiril@kentico.com>
2019-11-02 18:04:56 +0100

    Move first version of build client into docs to get published on the site

---
+++ docs/precache-manifest.c1c22e5c88779bb8cfa39d97112fe503.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "3545d995d5bf1de6f9676dbf55658baa",
+    "url": "/robomission/static/media/icon-diamond.3545d995.svg"
+  },
+  {
+    "revision": "8a3552752c7ebab72305",
+    "url": "/robomission/static/css/main.47cd9e56.chunk.css"
+  },
+  {
+    "revision": "8705e02d2b8ca811b3c7",
+    "url": "/robomission/static/js/runtime~main.9ad9f762.js"
+  },
+  {
+    "revision": "379c3955f0c98775d4732d0ca2b8a6fd",
+    "url": "/robomission/static/media/wormhole4.379c3955.png"
+  },
+  {
+    "revision": "e131d098e37300e388dc",
+    "url": "/robomission/static/js/2.bf082630.chunk.js"
+  },
+  {
+    "revision": "e1c2d5db58ad0a9af067ddd935f90fd1",
+    "url": "/robomission/static/media/background-space.e1c2d5db.png"
+  },
+  {
+    "revision": "177310c4d17a0b34870fa1fd6f4cbdd4",
+    "url": "/robomission/static/media/background-green.177310c4.png"
+  },
+  {
+    "revision": "bfe8d1cf7697c341502f4df477fe70cd",
+    "url": "/robomission/static/media/background-red.bfe8d1cf.png"
+  },
+  {
+    "revision": "8a3529bd94d5e29ac2212a2c47e0f8c8",
+    "url": "/robomission/static/media/background-yellow.8a3529bd.png"
+  },
+  {
+    "revision": "50dea97d719d02aa505b8ecb22eae54c",
+    "url": "/robomission/static/media/fi-slide.50dea97d.jpg"
+  },
+  {
+    "revision": "d4681479487ed91a35e0aa0d324d3e51",
+    "url": "/robomission/static/media/neurons-tile.d4681479.png"
+  },
+  {
+    "revision": "169dc827c0789ad0f1850a20d0e312e0",
+    "url": "/robomission/static/media/spaceship-in-spaceworld.169dc827.png"
+  },
+  {
+    "revision": "0fe704061ea0483f7239c54ab8edf989",
+    "url": "/robomission/static/media/asteroid.0fe70406.png"
+  },
+  {
+    "revision": "70f305a8f1e7da9f9ad08519c9d6ea50",
+    "url": "/robomission/static/media/background-blue.70f305a8.png"
+  },
+  {
+    "revision": "c8a0ec852ad8c6685eb517ec7f2bb79b",
+    "url": "/robomission/static/media/explosion.c8a0ec85.png"
+  },
+  {
+    "revision": "8a3552752c7ebab72305",
+    "url": "/robomission/static/js/main.19a4978a.chunk.js"
+  },
+  {
+    "revision": "378ed8e960178765764f85001208e2a9",
+    "url": "/robomission/static/media/icon-energy.378ed8e9.svg"
+  },
+  {
+    "revision": "2ef17ed788c028bf410bcb09f9b9b3e2",
+    "url": "/robomission/static/media/icon-length.2ef17ed7.svg"
+  },
+  {
+    "revision": "2fed42b85916f87b188c5b092a06640e",
+    "url": "/robomission/static/media/menu-banner.2fed42b8.png"
+  },
+  {
+    "revision": "b394556c21d3fb4e6cf10bb1414b3453",
+    "url": "/robomission/static/media/spaceship-broken.b394556c.png"
+  },
+  {
+    "revision": "8399ced5ca3211ba34b2b91a74f93fae",
+    "url": "/robomission/static/media/spaceship-broken__blue.8399ced5.png"
+  },
+  {
+    "revision": "a5bb452d5bd215c57ad4150ea2349499",
+    "url": "/robomission/static/media/spaceship-broken__green.a5bb452d.png"
+  },
+  {
+    "revision": "b394556c21d3fb4e6cf10bb1414b3453",
+    "url": "/robomission/static/media/spaceship-broken__red.b394556c.png"
+  },
+  {
+    "revision": "42b730533a4308c8205783bc9c636ae6",
+    "url": "/robomission/static/media/spaceship-broken__yellow.42b73053.png"
+  },
+  {
+    "revision": "d9b48d27e49ca5aa100e162a18d70667",
+    "url": "/robomission/static/media/speedometer.d9b48d27.png"
+  },
+  {
+    "revision": "148189e359f2607dbf974582d0ec4fb9",
+    "url": "/robomission/static/media/wormhole.148189e3.png"
+  },
+  {
+    "revision": "e5206004bd597919b9e9573290d62a51",
+    "url": "/robomission/static/media/wormhole2.e5206004.png"
+  },
+  {
+    "revision": "53801960e958f4e1df6e70a89ef85790",
+    "url": "/robomission/static/media/wormhole3.53801960.png"
+  },
+  {
+    "revision": "e131d098e37300e388dc",
+    "url": "/robomission/static/css/2.7e202766.chunk.css"
+  },
+  {
+    "revision": "d9e034d516b5a4ae3afd564287fd8911",
+    "url": "/robomission/index.html"
+  }
+];
